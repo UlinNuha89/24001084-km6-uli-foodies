2e13753564b09311b7a7dd0ae2c21189a01fe172
UlinNuha89 <unuha89.un@gmail.com>
2024-03-29 23:47:13 +0700

    Finishing shared preference for grid home function

--- app/src/main/java/com/lynn/foodies/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/lynn/foodies/presentation/home/HomeViewModel.kt
@@ -1,20 +1,30 @@
 package com.lynn.foodies.presentation.home
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lynn.foodies.data.repository.CatalogRepository
 import com.lynn.foodies.data.repository.CategoryRepository
+import com.lynn.foodies.data.source.local.pref.UserPreference
+import com.lynn.foodies.data.source.local.pref.UserPreferenceImpl
 
 class HomeViewModel(
     private val categoryRepository: CategoryRepository,
-    private val catalogRepository: CatalogRepository
+    private val catalogRepository: CatalogRepository,
+    context: Context
 ) : ViewModel() {
+
+    fun getPref(context: Context): Boolean = UserPreferenceImpl(context).isUsingGridMode()
+    fun setPref(context: Context, isUsingGridMode: Boolean) =
+        UserPreferenceImpl(context).setUsingGridMode(isUsingGridMode)
 
     fun getCategory() = categoryRepository.getCategory()
     fun getCatalog() = catalogRepository.getCatalog()
 
-    private val _isUsingGridMode = MutableLiveData(true)
+    private val _isUsingGridMode = MutableLiveData(
+        getPref(context)
+    )
     val isUsingGridMode: LiveData<Boolean>
         get() = _isUsingGridMode
 
