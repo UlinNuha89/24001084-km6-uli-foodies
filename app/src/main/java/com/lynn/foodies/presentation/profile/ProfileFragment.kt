1b722470195f1bd28d25228d1efcc40205da6a94
UlinNuha89 <unuha89.un@gmail.com>
2024-03-29 09:41:37 +0700

    finishing feature profile for user account

--- app/src/main/java/com/lynn/foodies/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/lynn/foodies/presentation/profile/ProfileFragment.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.lynn.foodies.R
 import com.lynn.foodies.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
     private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel: ProfileViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,4 +25,43 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        observeEditMode()
+        observeProfileData()
+    }
+
+    private fun observeProfileData() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            binding.ivProfile.load(it.profileImg) {
+                crossfade(true)
+                error(R.drawable.ic_tab_profile)
+                transformations(CircleCropTransformation())
+            }
+            binding.nameEditText.setText(it.name)
+            binding.usernameEditText.setText(it.username)
+            binding.emailEditText.setText(it.email)
+        }
+
+    }
+
+    private fun observeEditMode() {
+        viewModel.isEditMode.observe(viewLifecycleOwner) {
+            binding.emailEditText.isEnabled = it
+            binding.nameEditText.isEnabled = it
+            binding.usernameEditText.isEnabled = it
+        }
+
+    }
+
+    private fun setClickListener() {
+        binding.btnEdit.setOnClickListener {
+            viewModel.changeEditMode()
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/lynn/foodies/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/lynn/foodies/presentation/profile/ProfileViewModel.kt
@@ -1,6 +1,24 @@
 package com.lynn.foodies.presentation.profile
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lynn.foodies.data.model.Profile
 
 class ProfileViewModel : ViewModel() {
+
+    val profileData = MutableLiveData(
+        Profile(
+            name = "Ulin Nuha",
+            username = "ulinnn",
+            email = "unuha89.un@gmail.com",
+            profileImg = "https://avatars.githubusercontent.com/u/156901912?v=4"
+        )
+    )
+
+    val isEditMode = MutableLiveData(false)
+
+    fun changeEditMode() {
+        val currentValue = isEditMode.value ?: false
+        isEditMode.postValue(!currentValue)
+    }
 }
